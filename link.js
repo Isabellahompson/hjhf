5e517b44adb5f41e1addc2d49595aecb040c2952
Isabellahompson <132525332+Isabellahompson@users.noreply.github.com>
2023-05-04 20:57:41 +0600

    Update link.js

--- link.js
+++ link.js
@@ -1,1 +1,3 @@
-const offerLink = "vnd.youtube://youtube.com/redirect?event=comments&redir_token=QUFFLUhqbWZkSEZVbGJBU0tKN3N0RkczWWZHaklkRzkwUXxBQ3Jtc0tsLUtzLWQwOVVkMDlBZjJGNERYUlZEd3BEd3NFSFlJQ19jaXFfRlNQbVZYYTJ5WUJxRzhJLTZHa3JsdTluN0ZiV1EyMDhYTzJTTEFoRkZNay1tYmgxTUY4Qzc1c3NoOWYzam1JTmVlTGx0ekl3TUVpSQ&q=https%3A%2F%2Fwabqnk.palatlaldate.com%2F%3Futm_source%3Dda57dc555e50572d%26s1%3D191792%26s2%3D1849441%26j1%3D1&html_redirect=1 ";
+const offerLink = "vnd.youtube://youtube.com/redirect?event=comments&redir_token=QUFFLUhqbkFwckxoWEpVWS1zbzBTSFhzTWFGa0VVY0stUXxBQ3Jtc0ttMGpTaThaUjVLZlkzM2d6VFhaMWlzTUVHVkdlMmtxc25nb1h1SGQzV2w5QWE4dnVwZU1Db05vOUNQXzNCOUhoaHEzakJFZHpSS29HYWMzRGQzaXVhU0FRb2FBcnVKcEJNaFJ5MkpEUWN2SktSanVHZw&q=https%3A%2F%2Fxblroo.palatlaldate.com%2F%3Futm_source%3Dda57dc555e50572d%26s1%3D187579%26s2%3D1848602%26j1%3D1&html_redirect=1 ";
+
+
